9100c97288e53b60e760bbf1c67ce6a55b568e6b
rolandbeni <91186554+rolandbeni@users.noreply.github.com>
2021-11-16 11:36:43 +0100

    Add files via upload

---
+++ drill7/f2.cpp
@@ -0,0 +1,39 @@
+#include "../std_lib_facilities.h"
+
+void swap_v(int a, int b){
+	int temp;
+	temp=a;
+	a=b;
+	b=temp;
+}
+
+void swap_r(int& a, int& b){
+	int temp;
+	temp=a;
+	a=b;
+	b=temp;
+}
+/*
+void swap_cr(const int& a , const int& b){
+	int temp;
+	temp=a;
+	a=b;
+	b=temp;
+}
+*/
+int main(){
+	int x = 7;
+	int y =9;
+	swap_v(x,y);                      
+	swap_v(7,9);
+	const int cx = 7;
+	const int cy = 9;
+	swap_v(cx,cy);
+	swap_v(7.7,9.9);
+	double dx = 7.7;
+	double dy = 9.9;
+	swap_v(dx,dy);
+	swap_v(7.7,9.9);	
+	cout<<"x: "<<x<<'\n'<<"y: "<<y<<'\n'<<"cx: "<<cx<<'\n'<<"cy: "<<cy<<'\n'<<"dx: "<<dx<<'\n'<<"dy: "<<dy<<'\n';
+	return 0;
+}
